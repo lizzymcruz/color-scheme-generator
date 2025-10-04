6b2f619078218de10f0b8b1c9c2f2679d44fafea
lizzymcruz <leizylmcruz@gmail.com>
2025-10-04 16:52:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-const getSchemeBtn =  document.getElementById("colorpicker-form")
+const colorPickerForm =  document.getElementById("colorpicker-form")
 const showColors = document.getElementById('show-colors-container')
+
+function getAndRenderColors(hex, mode){
+    fetch(`https://www.thecolorapi.com/scheme?hex=${color}&mode=${scheme}&count=5`)
+        .then(res => res.json())
+        .then(data => {
+            showColors.innerHTML = ""
+            
+            data.colors.forEach(color => {
+                const colorDiv = document.createElement('div')
+                colorDiv.style.backgroundColor = color.hex.value
+
+                const hexCode = document.createElement('p')
+                hexCode.textContent = color.hex.value
+                colorDiv.appendChild(hexCode)
+
+                showColors.appendChild(colorDiv)
+        })
+                                  
+    })
+}
+
+getAndRenderColors("000000", "monochrome")
 
 getSchemeBtn.addEventListener('submit', function(e){
     e.preventDefault()
@@ -7,21 +29,5 @@ getSchemeBtn.addEventListener('submit', function(e){
     const color = document.getElementById('select-color').value.slice(1)
     const scheme  = document.getElementById('select-scheme').value
 
-    fetch(`https://www.thecolorapi.com/scheme?hex=${color}&mode=${scheme}&count=5`)
-        .then(res => res.json())
-        .then(data => {
-            const showColors = document.getElementById('show-colors-container')
-            showColors.innerHTML = ""
-            
-            data.colors.forEach(color => {
-                const colorDiv = document.createElement('div')
-                colorDiv.style.backgroundColor = color.hex.value
-                showColors.appendChild(colorDiv)
-                const hexCode = document.createElement('p')
-                hexCode.textContent = color.hex.value
-                colorDiv.appendChild(hexCode)
-        })
-                                  
-    })
-     
+    getAndRenderColors(color, scheme)
 })
